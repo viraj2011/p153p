e09e83762babe8e028ba58babda56bf965fedc63
viraj2011 <107635321+viraj2011@users.noreply.github.com>
2024-04-11 16:58:44 +0530

    Add files via upload

---
+++ components/Rotation.js
@@ -0,0 +1,33 @@
+//Terrain Rotation
+AFRAME.registerComponent("island-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 }    
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation < 0.1) {
+          this.data.speedOfRotation += 0.01;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation > -0.1) {
+          this.data.speedOfRotation -= 0.01;
+        }
+      }
+    });
+  },
+
+
+  tick: function () {
+    var mapRotation = this.el.getAttribute("rotation");
+
+    mapRotation.y += this.data.speedOfRotation;
+
+    this.el.setAttribute("rotation", {
+      x: mapRotation.x,
+      y: mapRotation.y,
+      z: mapRotation.z
+    });
+  }
+});
